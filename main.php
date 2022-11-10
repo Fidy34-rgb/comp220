b4c24a16b47e02d20dd7d39f7031e5d44157eb13
Fidy34-rgb <113130124+Fidy34-rgb@users.noreply.github.com>
2022-11-10 11:28:00 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,5 +1,17 @@
 <?php 
 require_once("include.php");
+//Pair C function
+function findTextInFile($sourceText, $targetString)
+{
+	$pos = 0;
+	$outputString = ("String " . $targetString . " was not found.");
+	$foundString = ($targetString . " was found at position ");
+	$pos = strpos($sourceText, $targetString);
+	if($pos >= 0)
+		$outputString = $foundString . ($pos + 1);
+	return $outputString;
+}
+
 //main
 $mysqlObj = CreateConnectionObject();
 $TableName = "fontNames";
